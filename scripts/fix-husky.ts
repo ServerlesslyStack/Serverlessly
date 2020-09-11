4a368b85c343ed64d6bcc901ee07b787d6bc36d0
Sachin Shekhar <noreply.git@sachinshekhar.com>
2020-09-12 04:38:25 +0530

    env: setup typescript

--- scripts/fix-husky.ts
+++ scripts/fix-husky.ts
@@ -1,16 +1,16 @@
-const fs = require('fs');
-const path = require('path');
+import { readFileSync, writeFileSync } from 'fs';
+import * as path from 'path';
 
 const huskyScriptPath = path.join(__dirname, '../.git/hooks/husky.sh');
 
 // If Operating System is Windows, fix Husky for Yarn
 if (process.platform === 'win32') {
-  const huskyScript = fs.readFileSync(huskyScriptPath, {
+  const huskyScript = readFileSync(huskyScriptPath, {
     encoding: 'utf-8',
   });
   const fixedHuskyScript = huskyScript.replace(
     'run_command yarn run --silent;;',
     'run_command npx --no-install;;'
   );
-  fs.writeFileSync(huskyScriptPath, fixedHuskyScript);
+  writeFileSync(huskyScriptPath, fixedHuskyScript);
 }
