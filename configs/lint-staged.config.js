356965c2488ef077483e07f2cb01944eed90fe00
Sachin Shekhar <noreply.git@sachinshekhar.com>
2020-09-17 02:56:31 +0530

    env: update lint-staged & its config
    new version of lint-staged allow function based config which is better when using own globs

--- configs/lint-staged.config.js
+++ configs/lint-staged.config.js
@@ -14,27 +14,22 @@ function lint(files) {
   return `eslint ${files.join(' ')}`;
 }
 
-const lintStagedConfig = {
-  '*': (stagedFiles) => {
-    const jsFilesInPackages = mm(stagedFiles, [
-      '**/@(@serverlessly|packages)/**/*.js',
-    ]);
-    if (jsFilesInPackages.length) {
-      return `node -e "throw 'Javascript files are not allowed. Convert them to Typescript files.'"`;
-    }
+const lintStagedConfig = (stagedFiles) => {
+  const jsFilesInPackages = mm(stagedFiles, [
+    '**/@(@serverlessly|packages)/**/*.js',
+  ]);
+  if (jsFilesInPackages.length) {
+    return `node -e "throw 'Javascript files are not allowed. Convert them to Typescript files.'"`;
+  }
 
-    const codeFiles = mm(stagedFiles, ['*.ts', '*.js'], {
-      dot: true,
-      matchBase: true,
-    });
-    const jsonFiles = mm(stagedFiles, ['**/*.json']);
-    const docsFiles = mm(stagedFiles, ['**/*.md']);
+  const codeFiles = mm(stagedFiles, ['*.ts', '*.js'], {
+    dot: true,
+    matchBase: true,
+  });
+  const jsonFiles = mm(stagedFiles, ['**/*.json']);
+  const docsFiles = mm(stagedFiles, ['**/*.md']);
 
-    return [
-      format([...codeFiles, ...jsonFiles, ...docsFiles]),
-      lint(codeFiles),
-    ];
-  },
+  return [format([...codeFiles, ...jsonFiles, ...docsFiles]), lint(codeFiles)];
 };
 
 module.exports = lintStagedConfig;
