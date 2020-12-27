8fac93db8a32b160866ab21a7de685199d91736b
Sachin Shekhar <noreply.git@sachinshekhar.com>
2020-12-27 22:18:55 +0530

    test(core): add tests to complete coverage

--- @serverlessly/core/__tests__/protocol.test.ts
+++ @serverlessly/core/__tests__/protocol.test.ts
@@ -1,4 +1,40 @@
-import { protocolServerAdapter } from '@serverlessly/core';
+import { Protocol, protocolServerAdapter } from '@serverlessly/core';
+import { dummyMiddlewareEngineSync } from './dummies/middleware-engines';
+import { DummyMiddlewareSync } from './dummies/middlewares';
+import { DummyProtocolContextSync } from './dummies/protocol-contexts';
+import { dummyProtocolServerFactorySync } from './dummies/protocol-server-factories';
+import { DummyProtocolServerSync } from './dummies/protocol-servers';
+
+test('Protocol is initialized successfully', () => {
+  expect<
+    Protocol<
+      DummyProtocolContextSync,
+      DummyMiddlewareSync,
+      DummyProtocolServerSync,
+      string
+    >
+  >(
+    new Protocol({
+      name: 'Dummy Protocol',
+      middlewareEngine: dummyMiddlewareEngineSync,
+      serverFactory: dummyProtocolServerFactorySync,
+    })
+  ).toBeTruthy();
+});
+
+test('Initialized Protocol is correct object', () => {
+  expect(
+    new Protocol({
+      name: 'Dummy Protocol',
+      middlewareEngine: dummyMiddlewareEngineSync,
+      serverFactory: dummyProtocolServerFactorySync,
+    })
+  ).toMatchObject({
+    name: 'Dummy Protocol',
+    middlewareEngine: dummyMiddlewareEngineSync,
+    serverFactory: dummyProtocolServerFactorySync,
+  });
+});
 
 test('protocolServerAdapter is a passthrough Platform Adapter', () => {
   const foo = () => 'Foo';
